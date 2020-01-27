aae96c34d78aa64312c7a2dabbc9faea53c8e1c1
jcchung1 <42154590+jcchung1@users.noreply.github.com>
2020-01-27 12:51:41 -0800

    Update SimpleTransmitter.m

--- simple_TxRx/SimpleTransmitter.m
+++ simple_TxRx/SimpleTransmitter.m
@@ -6,13 +6,13 @@
 % .5 sec Ts
 % Carrier Freq 500 hz
 
-PFbits = 2;
-DataBits = 8;
-SFbits = 2;
+PFbits = 0;
+DataBits = 64;
+SFbits = 0;
 TotalBits = PFbits + DataBits + SFbits;
-Ts = .5;
+Ts = .1;
 Fc = 500;
-Fs = 8192;
+Fs = 44100;
 
 Prefix = zeros(PFbits,1);
 temp = 1;
@@ -21,6 +21,7 @@ for i = 1:PFbits
     temp = temp*(-1);
 end
 
+rng(1234)
 Data = (2.*round(rand(DataBits,1)))-1;
 
 
@@ -33,14 +34,20 @@ end
 
 signalbits = [Prefix; Data; Suffix];
 signal = repelem(signalbits,Fs*Ts);
+prefixsig = repelem(Prefix,Fs*Ts);
 
 carrier = zeros(length(signal),1);
 for i = 0:length(signal)-1
     carrier(i+1,1) = cos(Fc*(2*pi) * i / Fs);
 end
 
+prefixsig = prefixsig.*carrier(1:length(prefixsig));
 Tx = signal.*carrier;
-
+longRx = zeros(4*length(Tx),1);
+longRx(2000000:2000000+length(Tx)-1) = Tx;
+longRx = longRx + .01*randn(length(longRx),1);
 sound(Tx,Fs);
+
+cor = xcorr(longRx,prefixsig);
 
 Rx = Tx.*carrier;
