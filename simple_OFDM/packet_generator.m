5540216fc2f304edaddb383fd8ef0724723be58c
jcchung1 <42154590+jcchung1@users.noreply.github.com>
2020-02-03 13:16:25 -0800

    Update packet_generator.m

--- simple_OFDM/packet_generator.m
+++ simple_OFDM/packet_generator.m
@@ -3,12 +3,12 @@ function x = packet_generator(bits_per_sym)
     % STF
     S = (1 / sqrt(2)) * [0, 0, (1 + 1j), 0, 0, 0, (-1 - 1j), 0, 0, 0, (1 + 1j), 0, 0, 0, (-1 - 1j), 0, 0, 0, (-1 - 1j), 0, 0, 0, (1 + 1j), 0, 0, 0, ...
     0, 0, 0, 0, (-1 - 1j), 0, 0, 0, (-1 - 1j), 0, 0, 0, (1 + 1j), 0, 0, 0, (1 + 1j), 0, 0, 0, (1 + 1j), 0, 0, 0, (1 + 1j), 0, 0];
-
+    
     x_stf = zeros(1,160);
     for time_index = 1:length(x_stf)
         time_sample = 0;
         for freq_index = 1: length(S)
-            time_sample = time_sample + (S(freq_index) * exp(1j*2*pi*312.5*(10^3)*50*(10^(-9))*(time_index-1)*(freq_index-27)));
+            time_sample = time_sample + (S(freq_index) * exp(1j*2*pi*625*22.68*(10^(-6))*(time_index-1)*(freq_index-27)));
         end
         x_stf(time_index) = (1 / sqrt(12)) * time_sample;
     end
@@ -21,27 +21,22 @@ function x = packet_generator(bits_per_sym)
     for time_index = 1:length(x_ltf)
         time_sample = 0;
         for freq_index = 1: length(L)
-            time_sample = time_sample + (L(freq_index) * exp(1j*2*pi*312.5*(10^3)*50*(10^(-9))*(time_index-1)*(freq_index-27)));
+            time_sample = time_sample + (L(freq_index) * exp(1j*2*pi*625*22.68*(10^(-6))*(time_index-1)*(freq_index-27)));
         end
         x_ltf(time_index) = (1 / sqrt(52)) * time_sample;
     end
 
     M = bits_per_sym;
-    num_carriers = 64;
-    num_prefix = 16;
+    num_carriers = 32;
+    num_prefix = 8;
     num_ofdmsymbols = 12;
 
     % Data
     bits = randi([0,1],1,(num_ofdmsymbols*num_carriers*max(log2(M),1)));
-    if log2(M) > 0
-        syms = (qammod(bits',M,'gray','InputType','bit','UnitAveragePower',true))';
-        refpts = qammod((0:(M-1))',M);
-    else
-        syms = bpsk(bits);
-        refpts = complex([-1 1]);
-    end
-    figure;
+    syms = (pskmod(bits',M));%,'gray','InputType','bit','UnitAveragePower',true))';
     
+    figure;
+    refpts = pskmod((0:(M-1))',M);
     plot(syms,'co');
     hold on;
     plot(refpts,'r*');
