a0b8b4437384bdae92e358f92494bf586163d207
Alex Gilbert <alexrgilbert@Alexs-MacBook-Air.local>
2020-02-23 13:33:57 -0800

    sample rx signal

--- simple_OFDM/OFDM_RX.m
+++ simple_OFDM/OFDM_RX.m
@@ -3,6 +3,7 @@ clear all;
 clc;
 Fc = 500;
 Fs = 44100;
+us_rate = 2.25;
 nBits = 64;
 NumChannels = 1;
 filename = 'recording.wav';
@@ -32,23 +33,31 @@ y_bb_q = zeros(size(y));
 c = zeros(size(y));
 s = zeros(size(y));
 for i = 1:length(y)
+    % In Phase
     y_bb_i(i) = y(i) * sqrt(2)*cos(2*pi*FC*i/Fs);
     c(i) = sqrt(2)*cos(2*pi*FC*i/Fs);
-end
-for i = 1:length(y)
+    % Quadrature
     y_bb_q(i) = y(i) * -1 * sqrt(2) * sin(2*pi*FC*i/Fs);
-    s(i) = -sqrt(2)*(2*pi*FC*i/Fs);
+    s(i) = -sqrt(2)*sin(2*pi*FC*i/Fs);
 end
 
 y_bb = complex(y_bb_i,y_bb_q);
 
-y_s = zeros(1,(length(y)/Fs));
-for i = 1:Fs:(length(y)/Fs)
-    
-    for j = 1:Fs
-        
-    end
+    us_len = us_rate*length(x);
+    x_upsampled = complex(zeros(1,us_len),...
+                        zeros(1,us_len));
+ds_len = length(y_bb) / us_rate;
+y_bb_downsampled = complex(zeros(1,ds_len),...
+                    zeros(1,ds_len));
+
+for i = 1:(ds_len)
+    upsampled_idcs = (ceil((i-1)*2.25)+1):1:()(ceil((i)*2.25)+1)-1)
+    % % % TODO:  BETTER TO JUST CHOOSE ONE OR TAKE AVERAGE?
+    y_bb_downsampled(1,i) = (y_bb(1,upsampled_idcs(1)));
+    % y_bb_downsampled(1,i) = mean(y_bb(1,upsampled_idcs));
 end
 
+
+
 figure;
-plot(time_axis,y_bb);
+plot(time_axis,y_bb_downsampled);
